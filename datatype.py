e7118179108679822e838f960e5a225068f34f73
Zafar imam <zafarimam7032@gmail.com>
2024-12-14 23:21:48 +0530

    basic

---
+++ datatype.py
@@ -0,0 +1,19 @@
+a=20;
+b=30;
+print('result: ',a+b)
+
+name='zafar imam'
+
+print(name)
+
+check=True
+
+print(check)
+
+checkflag=False
+
+print(checkflag)
+
+salry=34.77
+
+print(salry)
---
+++ first.py
@@ -0,0 +1,7 @@
+print("this is first pyathon programming to print data")
+print('this is another way to print data');
+print('''
+this is way we can print 
+      multiple line data 
+      thats why 
+''')
