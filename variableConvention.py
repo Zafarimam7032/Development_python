255e344d8548b9af2253d6ada19f262cfe10676c
Zafar imam <zafarimam7032@gmail.com>
2024-12-21 15:00:29 +0530

    python

---
+++ variableConvention.py
@@ -0,0 +1,16 @@
+name="python developer"
+
+print(name)
+
+employee_name="zafar imam"
+
+print(employee_name)
+
+_salry=2000.0
+
+print(_salry)
+
+id12Number=2332
+
+print(id12Number)
+# 12data="masksa" this is not possible 
