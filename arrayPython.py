d827228b1ba901b61e865c8f3e8991af3c916f21
Zafar imam <zafarimam7032@gmail.com>
2025-02-16 22:51:27 +0530

    loop in python

---
+++ arrayPython.py
@@ -0,0 +1,6 @@
+
+import array as arr
+
+arrInt=arr.array('i',[1,2,3,4,5,6]);
+
+print(arrInt)
---
+++ loopPython.py
@@ -0,0 +1,12 @@
+
+list1=["zafar","meena","kirti","imam","manoj"];
+
+for name in list1:
+  print(name)
+else:
+  print("data print successfully")  
+
+i=0;
+while(i <len(list1)):
+  print(list1[i]) 
+  i+=1
