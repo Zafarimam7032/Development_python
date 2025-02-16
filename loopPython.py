9f15ab46ad8833d29e542d46c17d3305f791081d
Zafar imam <zafarimam7032@gmail.com>
2025-02-16 22:57:17 +0530

    nestle loop

--- loopPython.py
+++ loopPython.py
@@ -10,3 +10,14 @@ i=0;
 while(i <len(list1)):
   print(list1[i]) 
   i+=1
+
+list2=[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+
+for row in list2:
+  for element in row:
+    print(element,end=' ')
+  print()  
