e78279ddaa3b5164c7fe0b4d8125ae6b09956c7a
Zafar imam <zafarimam7032@gmail.com>
2025-05-09 12:18:57 +0530

    function data type

---
+++ PythonFunction.py
@@ -0,0 +1,16 @@
+def display():
+  print("this is first function")
+
+display()
+
+def addition():
+  a=10
+  b=30
+  return a+b;
+
+print(addition())
+
+def functionWithReturn(a,b):
+  return a+b;
+
+print(functionWithReturn(20,30))
---
+++ dictionaryDataType.py
@@ -0,0 +1,15 @@
+dictionAry={
+  "name":"zafar imam",
+  "address":"Siwan Bihar",
+  "Mobile":"123432",
+  "id":1232
+}
+
+print(dictionAry)
+print(dictionAry.keys())
+print(dictionAry.values())
+
+dct= dict("name","meena",
+          "id","1233")
+
+print(dct)
